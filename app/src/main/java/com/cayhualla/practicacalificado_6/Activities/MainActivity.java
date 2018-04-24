bca7ef4e49b250d3e2fa6fd65fd17e2be175efac
liset08 <liset.cayhualla@tecsup.edu.pe>
2018-04-24 08:44:00 -0500

    cambios

--- app/src/main/java/com/cayhualla/practicacalificado_6/Activities/MainActivity.java
+++ app/src/main/java/com/cayhualla/practicacalificado_6/Activities/MainActivity.java
@@ -3,11 +3,17 @@ package com.cayhualla.practicacalificado_6.Activities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cayhualla.practicacalificado_6.R;
 import com.cayhualla.practicacalificado_6.models.User;
@@ -24,6 +30,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        getSupportActionBar().setTitle("Catalogo");
+
+        //menu bottom
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_home:
+                        Toast.makeText(MainActivity.this, "Go home section...", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_camera:
+                        Toast.makeText(MainActivity.this, "Go camera section...", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_search:
+                        Toast.makeText(MainActivity.this, "Go share section...", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+                return true;
+            }
+        });
 
         usernameText1 = (TextView)findViewById(R.id.welcome_text);
 
@@ -43,7 +74,51 @@ public class MainActivity extends AppCompatActivity {
         usernameText.setText(user.getFullname());
     }
 
-    public void callLogout(View view){
+
+
+
+    //menu superior
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+
+            case R.id.action_takepicture:
+                Toast.makeText(this, "Tomando una foto...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_logout:
+
+               callLogout();
+                Toast.makeText(this, "Cerrando Sesion...", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_toggle:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    Toast.makeText(this, "Modo offline desactivado...", Toast.LENGTH_SHORT).show();
+                }else{
+                    item.setChecked(true);
+                    Toast.makeText(this, "Modo offline activado...", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            case R.id.action_about:
+                Toast.makeText(this, "Acerca de...", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void callLogout(){
         // remove from SharedPreferences
         SharedPreferences.Editor editor = sharedPreferences.edit();
         boolean success = editor.putBoolean("islogged", false).commit();
@@ -59,4 +134,5 @@ public class MainActivity extends AppCompatActivity {
         finish();
 
     }
+
 }
